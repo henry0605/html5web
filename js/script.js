b7db5b3a8c483e9de0625265ba2a471aeb9db906
henry0605 <jiasheng0605@gmail.com>
2024-08-12 17:03:52 +0800

    0812

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 //移除行動裝置背景影片
-$(function () {
+$( function () {   /* document ready 一個js檔只要執行一次就可以 */
+    //移除行動裝置的背景影片
     if ($(window).width() < 821) {
         $('abort video') .remove();
     }
+
+    // 交替漢堡按鈕
+    $('.hamburger').click(function(){
+        //.hamburger 交替切換 .is-active
+        $(this).toggleClass('is-active');
+        //.navigation 交替切換 .show 
+        $('.navigation').toggleClass('show');
+    });
+
+    //滑動至指定位置
+    $('.menu a').click(function(){
+        let btn = $(this).attr('href'); //返回取得屬性與值
+        let pos = $(btn).offset(); //抓取相對的座標位置
+        $('html,body').animate({scrollTop:pos.top},500);
+    });
 
 });
